1a042cfc8716a8abc8719d3a5175c65a75b6e3a1
abhinay2030 <yadavabhinay241@gmail.com>
2025-08-21 16:18:57 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,13 @@
+def is_prime(num):
+   
+    if num <= 1:
+        return False  
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False 
+    return True  
+
+print("Prime numbers between 1 and 100 are:")
+for number in range(1, 101):
+    if is_prime(number):
+        print(number, end=" ")
